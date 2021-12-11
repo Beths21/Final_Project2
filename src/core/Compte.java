fb83a2d933ddcc93b96e46248670946e31644a38
Beths21 <bethsvaldemar@gmail.com>
2021-12-11 13:18:21 -0500

    modifier a nouveau

--- src/core/Compte.java
+++ src/core/Compte.java
@@ -1,10 +1,14 @@
 package core;
 
-public class Compte {private ArrayList<Integer> numero;
-private ArrayList<String> client;
-private ArrayList<String> monnaie;
-private ArrayList<Integer> solde;
-private ArrayList<String> etat;
+
+import java.util.ArrayList;
+
+public class Compte {
+	private static ArrayList<Integer> numero=     new ArrayList<Integer>();
+	private static ArrayList<String> type =    new ArrayList<String>();
+	private static ArrayList<String> monnaie=   new ArrayList<String>();
+	private static ArrayList<Integer> solde =  new ArrayList<Integer>();
+	private static ArrayList<String> etat   = new ArrayList<String>();
 
 
 
@@ -13,81 +17,93 @@ private ArrayList<String> etat;
 
 
 
-public ArrayList<Integer> getNumero() {
-	return numero;
-}
+	public ArrayList<Integer> getNumero() {
+		return numero;
+	}
 
 
 
 
-public void setNumero(ArrayList<Integer> numero) {
-	this.numero = numero;
-}
+	public void setNumero(ArrayList<Integer> numero) {
+		this.numero = numero;
+	}
 
 
 
 
-public ArrayList<String> getClient() {
-	return client;
-}
+	public ArrayList<String> getType() {
+		return type;
+	}
 
 
 
-public void setClient(ArrayList<String> client) {
-	this.client = client;
-}
-
-
-
-
-public ArrayList<String> getMonnaie() {
-	return monnaie;
-}
-
-
-
-public void setMonnaie(ArrayList<String> monnaie) {
-	this.monnaie = monnaie;
-}
-
-
-
-public ArrayList<Integer> getSolde() {
-	return solde;
-}
-
-
-
-public void setSolde(ArrayList<Integer> solde) {
-	this.solde = solde;
-}
-
-
-public ArrayList<String> getEtat() {
-	return etat;
-}
-
-
-public void setEtat(ArrayList<String> etat) {
-	this.etat = etat;
-}
+	public void setType(ArrayList<String> type) {
+		this.type = type;
+	}
 
 
 
 
-
-public Compte(ArrayList<Integer> numero, ArrayList<String> client, ArrayList<String> monnaie,
-		ArrayList<Integer> solde, ArrayList<String> etat) {
-	super();
-	this.numero = numero;
-	this.client = client;
-	this.monnaie = monnaie;
-	this.solde = solde;
-	this.etat = etat;
-}
+	public ArrayList<String> getMonnaie() {
+		return monnaie;
+	}
 
 
 
+	public void setMonnaie(ArrayList<String> monnaie) {
+		this.monnaie = monnaie;
+	}
+
+
+
+	public ArrayList<Integer> getSolde() {
+		return solde;
+	}
+
+
+
+	public void setSolde(ArrayList<Integer> solde) {
+		this.solde = solde;
+	}
+
+
+	public ArrayList<String> getEtat() {
+		return etat;
+	}
+
+
+	public void setEtat(ArrayList<String> etat) {
+		this.etat = etat;
+	}
+
+
+
+
+
+	public Compte(ArrayList<Integer> numero, ArrayList<String> type, ArrayList<String> monnaie,
+			ArrayList<Integer> solde, ArrayList<String> etat) {
+		super();
+		this.numero = numero;
+		this.type = type;
+		this.monnaie = monnaie;
+		this.solde = solde;
+		this.etat = etat;
+	}
+
+
+	public static void Enregistrer(int num, int sol,String typ, String mon, String et) {
+		numero.add(num);
+		solde.add(sol);
+		type.add(typ);
+		monnaie.add(mon);
+		etat.add(et);
+		
+		if(numero.contains(num) && solde.contains(sol) && type.contains(typ) && monnaie.contains(mon) && etat.contains(et)) {
+			System.out.println("Enregistrer avec succes");
+			
+		}
+		else
+		System.out.println("non enregistrer");
+	}
+	}
 	
-
-}
--- src/core/PlayBook.java
+++ src/core/PlayBook.java
@@ -1,9 +1,25 @@
 package core;
 
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PlayBook {
 	public static void main(String[]args)
     {
-   	 
+
+		
+			Scanner src = new Scanner(System.in);
+	  int num1= src.nextInt();
+	  int sol1= src.nextInt();
+	  String mon1= src.next();
+	  String type1= src.next();
+	  String etat1= src.next();
+	  
+	  
+	  
+	  Compte.Enregistrer(num1, sol1,type1, mon1, etat1);
+	  
     }
 
 }
